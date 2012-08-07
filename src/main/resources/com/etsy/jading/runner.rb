d48ed05e0b7cf44e34e081ede287b107e6656c37
Matt Walker <mwalker@etsy.com>
2012-08-07 01:22:58 -0500

    Suggestion from Ben Linsay: complete all cascades in the global registry, not just the last one

--- src/main/resources/com/etsy/jading/runner.rb
+++ src/main/resources/com/etsy/jading/runner.rb
@@ -1,9 +1,10 @@
 puts "Found #{Cascading::Cascade.all.size} Cascades in global registry"
 
-cascade_name = Cascading::Cascade.all.last.name
-puts "Jading is running the '#{cascade_name}' Cascade"
+# All cascades are run with shared properties and ARGV
+Cascading::Cascade.all.each do |cascade|
+  puts "Jading is running the '#{cascade.name}' Cascade"
+  cascade = Cascading::Cascade.get(cascade.name)
 
-cascade = Cascading::Cascade.get(cascade_name)
-
-# $jobconf_properties is bound in com.etsy.jading.Main#run(Properties, String[])
-cascade.complete($jobconf_properties)
+  # $jobconf_properties is bound in com.etsy.jading.Main#run(Properties, String[])
+  cascade.complete($jobconf_properties)
+end
