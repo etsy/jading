be70ad9eef061d8e4c1c1aa91df76365667557f8
Andrew Morrison <amorrison@etsy.com>
2015-09-23 11:56:18 -0400

    Archiving project. Removing all files.

--- src/main/resources/com/etsy/jading/runner.rb
+++
@@ -1,7 +0,0 @@
-puts "Found #{Cascading::Cascade.all.size} Cascades in global registry"
-
-# All cascades are run with shared ARGV but copies of the global $jobconf_properties
-Cascading::Cascade.all.each do |cascade|
-  puts "Jading is running the '#{cascade.name}' Cascade"
-  Cascading::Cascade.get(cascade.name).complete
-end
